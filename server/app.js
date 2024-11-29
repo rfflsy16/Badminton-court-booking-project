4e1c27b90edd530425d022759bd9966e6b05784f
rfflsy16 <rfflsy16@gmail.com>
2024-11-29 16:00:39 +0700

    change

--- server/app.js
+++ server/app.js
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 import cors from "cors";
 import { router } from "./router/index.js"
 const app = express();
+dotenv.config();
 const port = process.env.PORT || 3003;
 
-dotenv.config();
 
 app.use(cors());
 app.use(express.json());

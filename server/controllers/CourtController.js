ab3da0797231c53ac6c085e36950b91b96fcd098
rfflsy16 <rfflsy16@gmail.com>
2024-12-01 05:35:24 +0700

    feat: commited court

--- server/controllers/CourtController.js
+++ server/controllers/CourtController.js
@@ -1,3 +1,61 @@
-export default class CourtController {
+import CourtModel from "../models/court";
 
+export default class CourtController {
+    static async getAll(req, res, next) {
+        try {
+            const courts = await CourtModel.readCourts();
+            res.status(200).json(courts);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    static async getById(req, res, next) {
+        try {
+            const { id } = req.params;
+            const court = await CourtModel.readByIdCourt(id);
+            res.status(200).json(court);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    static async create(req, res, next) {
+        try {
+            const court = await CourtModel.createNewCourt(req.body);
+            res.status(201).json(court);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    static async update(req, res, next) {
+        try {
+            const { id } = req.params;
+            const court = await CourtModel.updateCourt(id, req.body);
+            res.status(200).json(court);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    static async delete(req, res, next) {
+        try {
+            const { id } = req.params;
+            const result = await CourtModel.deleteCourt(id);
+            res.status(200).json(result);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    static async getCourtsByBuilding(req, res, next) {
+        try {
+            const { BuildingId } = req.params;
+            const courts = await CourtModel.findCourtsByBuilding(BuildingId);
+            res.status(200).json(courts);
+        } catch (error) {
+            next(error);
+        }
+    }
 }
--- server/models/court.js
+++ server/models/court.js
@@ -1,29 +1,136 @@
+import { db } from "../config/mongoDB.js";
 import { ObjectId } from "mongodb";
-import { db } from "../config";
 
 export default class CourtModel {
     static getCollection() {
-        return db.collection('Courts')
+        return db.collection("Courts");
     }
 
-    static async readCourt() {
-        const collection = this.getCollection()
-        const courts = await collection.find().toArray()
-
-        return courts
+    static async readCourts() {
+        const collection = this.getCollection();
+        const courts = await collection.find().toArray();
+        return courts;
     }
 
-    static async readByidCourt(id) {
-        const _id = new ObjectId(id)
-        const collection = this.getCollection()
+    static async readByIdCourt(id) {
+        const _id = new ObjectId(id);
+        const collection = this.getCollection();
 
-        const court = await collection.findOne({ _id })
-        if (!court) throw { name: 'NotFound' }
+        const court = await collection.findOne({ _id });
+        if (!court) {
+            throw { name: "CourtNotFound" };
+        }
 
-        return court
+        return court;
     }
 
     static async createNewCourt(body) {
-        const { } = body
+        const {
+            BuildingId,
+            category,
+            type,
+            description,
+            startTime,
+            endTime,
+            excludedTime,
+            excludedDate,
+            price,
+            dp,
+        } = body;
+
+        if (
+            !BuildingId ||
+            !category ||
+            !type ||
+            !startTime ||
+            !endTime ||
+            !price ||
+            !dp
+        ) {
+            throw { name: "BADREQUEST" };
+        }
+
+        const collection = this.getCollection();
+        const newCourt = {
+            BuildingId: new ObjectId(BuildingId),
+            category,
+            type,
+            description,
+            startTime,
+            endTime,
+            excludedTime: excludedTime || [],
+            excludedDate: excludedDate || [],
+            price,
+            dp,
+            rating: [],
+            review: [],
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        };
+
+        const result = await collection.insertOne(newCourt);
+        return result.ops[0];
+    }
+
+    static async updateCourt(id, body) {
+        const _id = new ObjectId(id);
+        const {
+            category,
+            type,
+            description,
+            startTime,
+            endTime,
+            excludedTime,
+            excludedDate,
+            price,
+            dp,
+        } = body;
+
+        const updateData = {
+            ...(category && { category }),
+            ...(type && { type }),
+            ...(description && { description }),
+            ...(startTime && { startTime }),
+            ...(endTime && { endTime }),
+            ...(excludedTime && { excludedTime }),
+            ...(excludedDate && { excludedDate }),
+            ...(price && { price }),
+            ...(dp && { dp }),
+            updatedAt: new Date(),
+        };
+
+        const collection = this.getCollection();
+        const result = await collection.findOneAndUpdate(
+            { _id },
+            { $set: updateData },
+            { returnDocument: "after" }
+        );
+
+        if (!result.value) {
+            throw { name: "CourtNotFound" };
+        }
+
+        return result.value;
+    }
+
+    static async deleteCourt(id) {
+        const _id = new ObjectId(id);
+        const collection = this.getCollection();
+        const result = await collection.deleteOne({ _id });
+
+        if (result.deletedCount === 0) {
+            throw { name: "CourtNotFound" };
+        }
+
+        return { message: "Court deleted successfully" };
+    }
+
+    static async findCourtsByBuilding(BuildingId) {
+        const collection = this.getCollection();
+        const courts = await collection
+            .find({ BuildingId: new ObjectId(BuildingId) })
+            .toArray();
+
+        return courts;
     }
 }

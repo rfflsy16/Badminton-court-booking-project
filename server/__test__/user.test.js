3014a87d44f03798037a3cfc59c79e83d3a117e8
rfflsy16 <rfflsy16@gmail.com>
2024-12-02 15:44:58 +0700

    fix coverage

--- server/__test__/user.test.js
+++ server/__test__/user.test.js
@@ -117,4 +117,17 @@ describe('POST /login', () => {
             expect(response.body.message).toBe('Invalid email or password')
         })
     })
+    describe('POST /login - failed', () => {
+        it('should be return an error message because email or password is invalid', async () => {
+            const response = await request(app)
+                .post('/login')
+                .send({
+                    email: '',
+                    password: '123456'
+                })
+
+            expect(response.status).toBe(401)
+            expect(response.body.message).toBe('Please input email or password')
+        })
+    })
 })
